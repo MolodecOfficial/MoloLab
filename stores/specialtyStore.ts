a0a774f28aeb574cccb6f8c1d5184b036b77b760
Moloдец <maksimkq1990@mail.ru>
2024-12-11 01:46:44 +0500

    Update 0.12!

--- stores/specialtyStore.ts
+++ stores/specialtyStore.ts
@@ -3,31 +3,32 @@ export const specialtyList = [
         id: '1',
         specialty_name: 'Искусственный интеллект и цифровая трансформация бизнеса',
         group: 'БПИ, БПИ09-24-01',
-        code: 'БПИ, БПИ09',
-        direction: '09.03.03'
+        direction: 'БПИ, БПИ09',
+        code: '09.03.03',
+        faculty: 'ФЗО'
     },
     {
         id: '2',
         specialty_name: 'Нефтегазовые техника и технологии',
         group: 'САГ-24-01',
-        code: 'САГ',
-        direction: '21.05.06',
+        direction: 'САГ',
+        code: '21.05.06',
         faculty: 'ИНИЦТ'
     },
     {
         id: '3',
         specialty_name: 'Строительство уникальных зданий и сооружений',
         group: 'ССЗ-24-01',
-        code: 'ССЗ',
-        direction: '08.05.01',
+        direction: 'ССЗ',
+        code: '08.05.01',
         faculty: 'АСИ'
     },
     {
         id: '4',
         specialty_name: 'Управление качеством продукции нефтегазохимических и строительных производств',
         group: 'БОХ-24-01',
-        code: 'БОХ',
-        direction: '04.03.01',
+        direction: 'БОХ',
+        code: '04.03.01',
         faculty: 'ТФ'
     },
 ];
