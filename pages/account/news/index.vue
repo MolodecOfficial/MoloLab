bd671dc4a0a43175888781d24e9b230befaf7501
Moloдец <maksimkq1990@mail.ru>
2025-01-15 19:56:52 +0500

    Update 0.14!!

--- pages/account/news/index.vue
+++ pages/account/news/index.vue
@@ -23,7 +23,7 @@ useThemeStore()
 const MyNews: Array<LeftNews> = [
   {
     title: 'Обновление 0.14 (Установлено)',
-    link: 'https://t.me/molodec_official_tg/345'
+    link: 'https://t.me/molodec_official_tg/357'
   },{
     title: 'Обновление 0.13',
     link: 'https://t.me/molodec_official_tg/345'
