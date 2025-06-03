49f899295b2b1ac0e997b30a631c67b7cd16fa79
MolodecOfficial <maksimkq1990@mail.ru>
2025-06-03 19:23:14 +0500

    update 0.2 part 11

--- pages/student/account/index.vue
+++ pages/student/account/index.vue
@@ -310,7 +310,7 @@ onMounted(() => {
       </section>
     </NuxtLink>
 
-    <NuxtLink class="student-userdata" v-if="status === 'Администратор' || status === 'Владелец'" to="/adminPanel">
+    <NuxtLink class="student-userdata" v-if="status === 'Администратор' || status === 'Владелец' || status === 'Преподаватель'" to="/adminPanel">
       <section class="span">
         <svg fill="none" height="20" viewBox="0 0 16 20" width="20" xmlns="http://www.w3.org/2000/svg">
           <path
